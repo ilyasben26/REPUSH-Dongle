782fc67f8f4ac32f35492771dddf7108312907de
ilyasben26 <benyamnailyas@gmail.com>
2026-04-03 08:01:57 +0200

    feat: declutter main.cpp

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 screen /dev/cu.usbmodem1101 115200
 # challenge <tt> <bt> <tc> <bc> <count> [show_summary]
 debug_on
-challenge 1 6 0 7 1
+challenge 1 6 0 7 100 2
+challenge 2 0 0 2 100 1
+challenge 3 0 1 4 100 1
 # (303826966) (303826966)
 ```
---
+++ include/puf_functions.h
@@ -0,0 +1,19 @@
+#ifndef PUF_FUNCTIONS_H
+#define PUF_FUNCTIONS_H
+
+#include <Arduino.h>
+#include <vector>
+#include <map>
+
+extern bool debug_mode;
+
+void build_payload(uint8_t command, uint64_t data_value, uint8_t *payload);
+void send_command(const uint8_t *payload);
+bool read_response(uint8_t *response, unsigned long timeout = 2000);
+uint64_t bytes_to_uint64(const uint8_t *bytes);
+void print_binary(uint64_t value, int bits);
+void send_setup_and_wait(uint8_t command, uint64_t data_value, unsigned long delay_ms);
+bool request_puf_response(uint64_t &puf_value, unsigned long req_delay_ms);
+void execute_challenge(int top_tune, int bottom_tune, int top_choice, int bottom_choice, int count, int resp_delay_ms);
+
+#endif // PUF_FUNCTIONS_H
