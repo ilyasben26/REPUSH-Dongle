b714d7e77756d85f5914de60ea2906959940f9aa
ilyasben26 <benyamnailyas@gmail.com>
2026-07-15 10:35:22 +0100

    fix: remove unused code

--- include/puf_functions.h
+++ include/puf_functions.h
@@ -2,23 +2,9 @@
 #define PUF_FUNCTIONS_H
 
 #include <Arduino.h>
-#include <array>
-#include <vector>
-#include <map>
 
 extern bool debug_mode;
 
-std::array<uint8_t, 32> challenge_lr_puf(int challenge, int state_index, int count, int resp_delay_ms);
 uint32_t generateRandomSeed();
-void build_payload(uint8_t command, uint64_t data_value, uint8_t *payload);
-void send_command(const uint8_t *payload);
-bool read_response(uint8_t *response, unsigned long timeout = 2000);
-void bytes_to_array16(const uint8_t *bytes, std::array<uint8_t, 16> &out);
-void print_binary(uint64_t value, int bits);
-void send_setup_and_wait(uint8_t command, uint64_t data_value, unsigned long delay_ms);
-bool request_puf_response(std::array<uint8_t, 16> &puf_value, unsigned long req_delay_ms);
-bool execute_challenge(int top_tune, int bottom_tune, int top_choice, int bottom_choice, int count, int resp_delay_ms, std::array<uint8_t, 16> &majority_response);
-void find_valid_challenges();
-void reconfigure(int state_index);
 
 #endif // PUF_FUNCTIONS_H
